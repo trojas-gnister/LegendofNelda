cbdef85c729933ca5e3df3cc275667e0a87ef14e
iskry <refocuslucifer@protonmail.com>
2023-01-07 22:36:30 -0600

    added animation logic. need to add attack logic

--- controller/game.js
+++ controller/game.js
@@ -1,4 +1,5 @@
 var scene = new Phaser.Scene("game");
+var player;
 
 const config = {
   width: 680,
@@ -22,41 +23,78 @@ scene.init = function() {
 	this.lives_text;
 };
 
+
+
+
 scene.preload = function() {
-	this.load.image('background', './assets/img/sprites/map.png');
-	this.load.image('player', './assets/img/sprites/adventurer-idle.png');
-	this.load.image('shroom', './assets/img/sprites/shroom.png');
+  this.load.image('background', './assets/img/sprites/map.png');  
+  this.load.image('shroom', './assets/img/sprites/shroom.png');
+  this.load.atlas("adventurer", "adventurer.png", "adventurer.json");
+
+
 };
 
 scene.create = function() {
    var bg = this.add.sprite(0, 0, 'background');
    bg.setOrigin(0,0);
 
-   this.scoreText = this.add.text(100, 16, 'score: '+this.score, { fontSize: '32px', fill: '#000' });
-   this.liveText = this.add.text(16, this.sys.game.config.height-50, 'Lives: ' + this.lives, {fontSize: '32px', fill: '#000'});
-
-   this.player = this.add.sprite(110, 210, 'player');
-   this.player.setScale(2.9);
+   this.scoreText = this.add.text(100, 16, 'score: '+this.score, { fontSize: '32px', fill: '#fff' });
+   this.liveText = this.add.text(16, this.sys.game.config.height-50, 'Lives: ' + this.lives, {fontSize: '32px', fill: '#fff'});
 
 
-   this.shroom = this.add.sprite(350, 235, 'shroom');
-   this.shroom.setScale(2.1);
+   
+   this.anims.create({
+    key: "idle",
+    frameRate: 7,
+    frames: this.anims.generateFrameNames("adventurer", {
+        prefix: "idle",
+        suffix: ".png",
+        start: 0,
+        end: 2,
+        zeroPad: 1
+    }),
+    repeat: -1
+    });
+
+    this.anims.create({
+    key: "run",
+    frameRate: 7,
+    frames: this.anims.generateFrameNames("adventurer", {
+        prefix: "run",
+        suffix: ".png",
+        start: 0,
+        end: 5,
+        zeroPad: 1
+    }),
+    repeat: -1
+});
+
+  player = this.add.sprite(100, 300, "adventurer")
+
+  player.play("idle");
+
+  this.shroom = this.add.sprite(500, 300, "shroom")
+  this.shroom.setScale(3.3);
+
 };
 
 
 
 
 scene.update = function() {
+
   var cursors = this.input.keyboard.createCursorKeys();
   if (cursors.right.isDown) {
-    this.player.x += this.speed;
+    player.x += this.speed;
+    player.play('run')
+    
   }
 
-  if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.shroom.getBounds())) {
-    this.lives--;
-  	this.liveText.setText("Lives: " + this.lives);
-  	this.end();
-  }
+  // if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.shroom.getBounds())) {
+  //   this.lives--;
+  // 	this.liveText.setText("Lives: " + this.lives);
+  // 	this.end();
+  // }
 
 };
 
