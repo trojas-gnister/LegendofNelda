f598a844002e2a56efb00dceab08c10523ba1a8b
iskry <refocuslucifer@protonmail.com>
2023-01-09 12:04:01 -0600

    converted index.html to handlebars. corrected login functionality

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const express = require('express');
-const app = express();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
 const hbs = exphbs.create();
 
 const PORT = process.env.PORT || 3001;
