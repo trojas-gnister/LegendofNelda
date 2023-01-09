207bd6920d0038984eb70ef7bc1e72de4ed001d3
Cesar Castillo <castillocesar2412@gmail.com>
2023-01-08 18:31:56 -0600

    made adjustments to the server.js code

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create();
 
 const PORT = process.env.PORT || 3001;
 
