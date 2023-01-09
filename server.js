3e7dd05b713bc4bba9a96d6350454bf433e07b89
iskry <refocuslucifer@protonmail.com>
2023-01-08 19:08:57 -0600

    fixed game.js link

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ sequelize.sync({ force: false }).then(() => {
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/controller', express.static(__dirname + '/controller'));
-
 
 app.listen(8080, () => {
   console.log('Server listening on http://localhost:8080');
