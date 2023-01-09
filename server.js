cf5003ff217882444e70bf0964f879aa7049eabc
Cesar Castillo <castillocesar2412@gmail.com>
2023-01-08 18:05:29 -0600

    added code to server.js

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const express = require('express');
 const app = express();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
 
 app.use(express.static(__dirname + '/public'));
 
