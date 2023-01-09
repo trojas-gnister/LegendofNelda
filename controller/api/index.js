8be8c1b5e6a7e921957ab749128a875f7c48e95c
Cesar Castillo <castillocesar2412@gmail.com>
2023-01-08 18:35:07 -0600

    deleted dupicate files and code for index.js

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const user = require('./user');
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/user', user);
 
 
