ee4f140dba4cfb6aa8f361411d48c0b21bfcb65c
iskry <refocuslucifer@protonmail.com>
2023-01-09 18:29:09 -0600

    added more modules

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,16 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
 }
 
+
+
 module.exports = sequelize;
+
+module.exports = {'production': {
+
+  'use_env_variable': 'JAWSDB_URL',
+  
+  'dialect': 'mysql'
+  
+  }}
