d420229435c45bc86dbab10bd8efbca851061062
iskry <refocuslucifer@protonmail.com>
2023-01-10 21:32:23 -0600

    heroku

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
