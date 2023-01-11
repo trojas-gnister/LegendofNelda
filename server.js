ed8fa0ac23206abee5e84e5023a6dbed287feec1
iskry <refocuslucifer@protonmail.com>
2023-01-10 20:46:47 -0600

    heroku

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + '/public' ));
+app.use('/public/img', express.static(__dirname + '/public/img' ));
 
 
 app.use(routes);
