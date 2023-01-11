3c58dd65f2fa9522d70914bec66109532cb5ea7b
iskry <refocuslucifer@protonmail.com>
2023-01-10 22:02:24 -0600

    changed urlencoded to false in server.js on line 38

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
